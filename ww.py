66ed958e32fd158b38696ced3a4b322eca776f58
Boostszn <31454551+boostszn@users.noreply.github.com>
2018-06-19 18:20:11 -0500

    Create ww.py

---
+++ ww.py
@@ -0,0 +1,66 @@
+#Made by @boostszn on twitter!
+#Remove all comments and input info
+#THIS IS NOT A PYTHON SCRIPT RENAME AND DONT USE THE ENCDOINGS
+ 
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+ 
+import com.thoughtworks.selenium.Selenium
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.WebDriver
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium
+import static org.junit.Assert.*
+import java.util.regex.Pattern
+import static org.apache.commons.lang3.StringUtils.join
+ 
+WebUI.openBrowser('https://www.katalon.com/')
+def driver = DriverFactory.getWebDriver()
+String baseUrl = "https://www.katalon.com/"
+selenium = new WebDriverBackedSelenium(driver, baseUrl)
+selenium.open("#") #Product link
+selenium.click("link=SELECT SIZE")
+selenium.click("link=#")  #size
+selenium.click("id=btAddCarrinho")
+selenium.click("link=CHECK OUT")
+selenium.click("//form[@id='loginCarrinho']/div[2]/div/div/span")
+selenium.click("id=userName")
+selenium.type("id=userName", "#") Email
+selenium.click("name=login")
+selenium.click("xpath=(//input[@name='guestType'])[2]")
+selenium.click("name=login")
+selenium.click("id=nomeR")
+selenium.type("id=nomeR", "#") Name
+selenium.type("id=moradaR", "#") #addy
+selenium.type("id=cidadeR", "#") CITY
+selenium.type("id=cpR", "#")ZIP
+selenium.click("id=telefoneR")
+selenium.type("id=telefoneR", "#") #phone number
+selenium.click("id=btCheckout")
+selenium.click("id=C003")
+selenium.type("id=C003", "#") #card number
+selenium.click("id=mes")
+selenium.select("id=mes", "label=#") #Enter month
+selenium.click("id=ano")
+selenium.select("id=ano", "label=#") #enter year
+selenium.click("id=C005")
+selenium.type("id=C005", "#") #cvv
+selenium.click("link=Submit")
